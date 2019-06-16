a984d0ca6ce98d3b0872dd5a670da1760e53fb76
Patrik Csak <patrik@csak.email>
2019-06-16 13:19:34 -0400

    Add index file

---
+++ index.ts
@@ -0,0 +1,5 @@
+import bb26Range from './source/bb26-range'
+import bb26ToDecimal from './source/bb26-to-decimal'
+import decimalToBb26 from './source/decimal-to-bb26'
+
+export { bb26Range, bb26ToDecimal, decimalToBb26 }
