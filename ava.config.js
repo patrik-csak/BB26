6b92fef59590dddbd26f0c6889ef3c065b6fccc8
Patrik Csak <p@trikcsak.com>
2023-05-10 16:33:25 -0700

    drop support for node.js v14 and update dependencies

--- ava.config.js
+++ ava.config.js
@@ -1,3 +1,7 @@
+// https://github.com/avajs/ava/blob/v5.2.0/docs/recipes/typescript.md#for-packages-with-type-module
+//
+// doesn't work with node.js v20
+// see https://github.com/TypeStrong/ts-node/issues/1997
 const config = {
 	extensions: {
 		ts: 'module',
