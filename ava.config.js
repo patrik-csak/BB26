78e115b4dc03a61f42bcc2e36453982996f73d78
patrik csak <p@trikcsak.com>
2024-03-14 14:01:06 -0700

    fix tests

--- ava.config.js
+++ ava.config.js
@@ -1,12 +1,8 @@
-// https://github.com/avajs/ava/blob/v5.2.0/docs/recipes/typescript.md#for-packages-with-type-module
-//
-// doesn't work with node.js v20
-// see https://github.com/TypeStrong/ts-node/issues/1997
 const config = {
 	extensions: {
 		ts: 'module',
 	},
-	nodeArguments: ['--loader=ts-node/esm'],
+	nodeArguments: ['--import=tsimp'],
 };
 
 export default config;
