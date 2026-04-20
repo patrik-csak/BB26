52789146d8ac6e53608266869a1f8b27ee1e105e
Patrik Csak <p@trikcsak.com>
2026-04-20 09:44:14 -0700

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,30 @@ import xo from 'xo';
 
 export default defineConfig([
 	globalIgnores(['distribution/', '.tsimp/']),
-	packageJson.configs.recommended,
-	packageJson.configs.stylistic,
-	...xo.xoToEslintConfig([{prettier: 'compat'}]),
+
 	{
-		languageOptions: {
-			parserOptions: {
-				projectService: {
-					// https://typescript-eslint.io/packages/parser/#allowdefaultproject
-					allowDefaultProject: ['test/*.ts', 'vitest.config.ts'],
+		files: ['**/*.{js,ts}'],
+		extends: [
+			xo.xoToEslintConfig([
+				{
+					prettier: 'compat',
+					languageOptions: {
+						parserOptions: {
+							projectService: {
+								// https://typescript-eslint.io/packages/parser/#allowdefaultproject
+								allowDefaultProject: ['test/*.ts', 'vitest.config.ts'],
+							},
+						},
+					},
 				},
-			},
+			]),
+		],
+		rules: {
+			'e18e/prefer-static-regex': 'off',
 		},
+	},
+	{
+		files: ['package.json'],
+		extends: [packageJson.configs.recommended, packageJson.configs.stylistic],
 	},
 ]);
