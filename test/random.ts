13e87b48bc118674af51432b90e9e7f06db19fc4
Patrik Csak <p@trikcsak.com>
2026-04-20 10:31:56 -0700

    increase test sample size

--- test/random.ts
+++ test/random.ts
@@ -4,7 +4,7 @@ import {random, range} from '../source/index.js';
 test('returns a valid string given only an upper bound', () => {
 	const validLetters = range('AAA');
 
-	for (let i = 0; i < 1000; ++i) {
+	for (let i = 0; i < 100_000; ++i) {
 		expect(validLetters.includes(random('ZZ'))).toBe(true);
 	}
 });
@@ -12,7 +12,7 @@ test('returns a valid string given only an upper bound', () => {
 test('returns a valid string given both upper and lower bounds', () => {
 	const validLetters = range('AA', 'AAA');
 
-	for (let i = 0; i < 1000; ++i) {
+	for (let i = 0; i < 100_000; ++i) {
 		expect(validLetters.includes(random('AA', 'ZZ'))).toBe(true);
 	}
 });
