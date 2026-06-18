4b6140ad71d6341ef8cc3def8d45977e7a5f0944
Patrik Csak <p@trikcsak.com>
2026-06-17 18:08:37 -0700

    xo 2 → 3

---
+++ xo.config.ts
@@ -0,0 +1,21 @@
+import e18e from '@e18e/eslint-plugin';
+import packageJson from 'eslint-plugin-package-json/experimental';
+import {type FlatXoConfig} from 'xo';
+
+const config: FlatXoConfig = [
+	// Global ignores must be in its own object
+	// https://github.com/xojs/xo/tree/v3.0.1#ignores
+	{
+		ignores: ['distribution/', 'package-lock.json'],
+	},
+
+	{
+		prettier: 'compat',
+	},
+
+	e18e.configs.recommended,
+	packageJson.configs.recommended,
+	packageJson.configs.stylistic,
+];
+
+export default config;
